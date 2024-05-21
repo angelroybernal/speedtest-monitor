f57c531e19fd47ed42009bf00abc4aee04c5ae2a
Angel Roy Bernal <angelroybernal@protonmail.com>
2024-05-21 14:34:54 -0500

    Flask reemplaza Streamlit

---
+++ dashboard/__init__.py
@@ -0,0 +1,37 @@
+import os
+import time
+import sqlite3
+from flask import Flask, jsonify, render_template
+
+import os
+app = Flask(__name__)
+
+_port = os.environ.get('PORT', 5000)
+
+@app.route('/')
+def dashboard():
+    con = sqlite3.connect("../monitor.db")
+    cur = con.cursor()
+    res = cur.execute("SELECT * FROM internet_speed")
+    data = res.fetchall()
+    con.close()
+
+    chart_data = {
+        'labels' : [sub[0] for sub in data],
+        'upload' : [sub[1] for sub in data],
+        'download' : [sub[2] for sub in data]
+    }
+
+    return render_template('index.html', chart_data=chart_data)
+
+@app.route('/api')
+def get_data():
+    con = sqlite3.connect("../monitor.db")
+    cur = con.cursor()
+    res = cur.execute("SELECT * FROM internet_speed")
+    data = jsonify(res.fetchall())
+    con.close()
+    return data
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=_port, debug=True)
